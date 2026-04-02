0f56f3aa55300f4dbab4f444e8364f80f582b817
niraj bam <nirajbam51@gmail.com>
2026-04-02 17:52:59 +0545

    first commit

---
+++ bin/dart_assingnment1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_assingnment1/dart_assingnment1.dart' as dart_assingnment1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_assingnment1.calculate()}!');
+}
---
+++ test/dart_assingnment1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_assingnment1/dart_assingnment1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
