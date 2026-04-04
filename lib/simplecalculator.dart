8d282728464539bfda9985310a15a988fc9204f3
240011 <166486446+240011@users.noreply.github.com>
2026-04-04 09:12:01 +0545

    third commit

---
+++ lib/simplecalculator.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+void main() {                    
+  print("Enter first number:");
+  double a = double.parse(stdin.readLineSync()!);
+  print("Enter operator (+, -, *, /, %):");
+  String op = stdin.readLineSync()!;
+  print("Enter second number:");
+  double b = double.parse(stdin.readLineSync()!);
+  switch (op) {
+    case '+':
+      print(a + b);
+      break;
+    case '-':
+      print(a - b);
+      break;
+    case '*':
+      print(a * b);
+      break;
+    case '/':
+      if (b != 0) {
+        print(a / b);
+      } else {
+        print("Cannot divide by zero");
+      } break;
+    case '%':
+      if (b != 0) {
+        print(a % b);
+      } else {
+        print("Cannot modulo by zero");
+      } break;
+    default:
+      print("Invalid operator");
+  }
+} 
